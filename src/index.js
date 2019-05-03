9fa3b80c4ec772d7d3aa9ea38a9e72e537cd759c
Irina <agent203@site69.ru>
2019-05-03 12:34:59 +0300

    m2-task1: add prop-types.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/app/app.jsx';
 
+const filmList = [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`];
+
 const init = () => {
   ReactDom.render(
-      <App />,
+      <App filmList={filmList} />,
       document.getElementById(`root`)
   );
 };
