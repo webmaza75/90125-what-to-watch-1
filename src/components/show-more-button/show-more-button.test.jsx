bfd29bc231bde179f09ae2cbd4f2dd774c5ab158
Irina <webmaza75@gmail.com>
2019-06-27 10:13:25 +0300

    m6-task2: fix show-more-button test.

--- src/components/show-more-button/show-more-button.test.jsx
+++ src/components/show-more-button/show-more-button.test.jsx
@@ -3,15 +3,38 @@ import renderer from 'react-test-renderer';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 
-import ShowMoreButton from './show-more-button.jsx';
+import {ShowMoreButton} from './show-more-button.jsx';
 import reducer from '../../reducers/reducer.js';
+import films from '../../mocks/films.js';
 
-it(`ShowMoreButton correctly renders`, () => {
-  const showMoreButton = renderer
-    .create(<Provider store={createStore(reducer)}>
-      <ShowMoreButton />
-    </Provider>)
-    .toJSON();
+describe(`ShowMoreButton`, () => {
+  it(`ShowMoreButton correctly renders when films more when limit to show`, () => {
+    const showMoreButton = renderer
+      .create(<Provider store={createStore(reducer)}>
+        <ShowMoreButton maxShowFilms={2} filmsGroup={films} />
+      </Provider>)
+      .toJSON();
 
-  expect(showMoreButton).toMatchSnapshot();
+    expect(showMoreButton).toMatchSnapshot();
+  });
+
+  it(`ShowMoreButton correctly renders when films count equal to limit to show`, () => {
+    const showMoreButton = renderer
+      .create(<Provider store={createStore(reducer)}>
+        <ShowMoreButton maxShowFilms={3} filmsGroup={films} />
+      </Provider>)
+      .toJSON();
+
+    expect(showMoreButton).toMatchSnapshot();
+  });
+
+  it(`ShowMoreButton doesn\`t render when films count less when limit to show`, () => {
+    const showMoreButton = renderer
+      .create(<Provider store={createStore(reducer)}>
+        <ShowMoreButton maxShowFilms={20} filmsGroup={films} />
+      </Provider>)
+      .toJSON();
+
+    expect(showMoreButton).toMatchSnapshot();
+  });
 });
